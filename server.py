e41766ec05d280e21571fed3986e946127f48c7b
iiazertyuiopii <romain.le-bail@eleves.ec-nantes.fr>
2015-02-10 15:12:23 +0100

    errors

--- server.py
+++
@@ -1,8 +0,0 @@
-import OSC
-
-c = OSC.OSCClient()
-c.connect(('127.0.0.1', 5005))   # connect to SuperCollider
-oscmsg = OSC.OSCMessage()
-oscmsg.setAddress("/startup")
-oscmsg.append('HELLO')
-c.send(oscmsg)
